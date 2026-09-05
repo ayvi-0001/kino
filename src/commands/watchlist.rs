6330f6b4d09ee64df9ce35da1ed3a9810ecdff5b
ayvi-0001 <alankvickers+github@gmail.com>
2026-09-05 12:21:17 -0700

    feat: parse entry parts

--- src/commands/watchlist.rs
+++ src/commands/watchlist.rs
@@ -449,6 +449,11 @@ impl WatchListContent {
         for (idx, entry) in self.entries.iter().enumerate() {
             let line = format!("{}. {}\n", idx + 1, entry);
 
+            // TODO(ayvi-0001): search movie on tmdb api
+            if let Ok(EntryParts(name, year)) = EntryParts::new(entry) {
+                tracing::info!("extracted entry parts: name={:#?}, year={:#?}", name, year);
+            }
+
             if self.body.len() + line.len() > budget {
                 break;
             }
@@ -474,3 +479,41 @@ impl WatchListContent {
     }
 }
 
+lazy_static::lazy_static! {
+    pub static ref ENTRY_REGEX: regex::Regex =
+        regex::Regex::new(r"^\s*(?<title>.+?)(?:\s+\(?(?<year>\d{4})\)?)?\s*$").unwrap();
+}
+
+#[derive(Debug)]
+pub(crate) struct EntryParts(pub(crate) String, pub(crate) Option<i64>);
+
+impl EntryParts {
+    pub fn new(text: &str) -> Result<EntryParts, anyhow::Error> {
+        let mut name: Option<String> = None;
+        let mut year: Option<i64> = None;
+
+        if let Some(caps) = ENTRY_REGEX.captures(text) {
+            if let Some(name_match) = caps.name("title") {
+                name = Some(name_match.as_str().to_owned());
+            }
+            if let Some(year_match) = caps.name("year") {
+                let year_str = year_match.as_str();
+                let year_trimmed = year_str
+                    .strip_prefix("(")
+                    .unwrap_or(year_str)
+                    .strip_suffix(")")
+                    .unwrap_or(year_str);
+
+                if let Ok(year_parsed) = year_trimmed.parse::<i64>() {
+                    year = Some(year_parsed);
+                }
+            }
+        }
+
+        if let Some(name) = name {
+            Ok(EntryParts(name, year))
+        } else {
+            Err(anyhow::anyhow!("Entry missing title"))
+        }
+    }
+}
